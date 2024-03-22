b8906bdb23d61d2ca046fc08c11bf8eb5d48c74c
Clay O'Dell <clay.odell.music@gmail.com>
2024-03-22 10:30:02 -0500

    completed weekday_name.py

--- 02_weekday_name/weekday_name.py
+++ 02_weekday_name/weekday_name.py
@@ -12,3 +12,8 @@ def weekday_name(day_of_week):
         >>> weekday_name(9)
         >>> weekday_name(0)
     """
+    days = ['None', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    if 1 <= day_of_week <= 7:
+        return days[day_of_week]
+    else:
+        return None
