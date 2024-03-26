49ef292f8f0a309219a6855b7f519435d8d39be2
Clay O'Dell <clay.odell.music@gmail.com>
2024-03-25 20:03:46 -0500

    completed partition.py

--- 16_partition/partition.py
+++ 16_partition/partition.py
@@ -19,3 +19,11 @@ def partition(lst, fn):
         >>> partition(["hi", None, 6, "bye"], is_string)
         [['hi', 'bye'], [None, 6]]
     """
+    pass_list = []
+    fail_list = []
+    for item in lst:
+        if fn(item):
+            pass_list.append(item)
+        else: 
+            fail_list.append(item)
+    return [pass_list, fail_list]
