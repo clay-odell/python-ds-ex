cd4c8e2eeb9ce3f426204a0e3ad2c404d0a3a3b2
Clay O'Dell <clay.odell.music@gmail.com>
2024-03-27 14:37:16 -0500

    completed calculate.py

--- 18_calculate/calculate.py
+++ 18_calculate/calculate.py
@@ -26,3 +26,21 @@ def calculate(operation, a, b, make_int=False, message='The result is'):
         >>> calculate('foo', 2, 3)
         
     """
+    if operation == 'add':
+        result = a + b
+    elif operation == 'subtract':
+        result = a - b
+    elif operation == 'multiply':
+        result = a * b
+    elif operation == 'divide':
+        if b != 0:
+            result = a / b
+        else:
+            return "Error: Dividing by zero isn't allowed"
+    else:
+        return None
+
+    if make_int:
+        result = int(result)
+
+    return f"{message} {result}"
