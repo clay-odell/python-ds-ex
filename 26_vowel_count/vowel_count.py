a369eaab75f85e32c0dd9ed62a36f97fc3df4b8c
Clay O'Dell <clay.odell.music@gmail.com>
2024-03-27 16:54:49 -0500

    vowel_count.py completed

--- 26_vowel_count/vowel_count.py
+++ 26_vowel_count/vowel_count.py
@@ -7,3 +7,8 @@ def vowel_count(phrase):
         >>> vowel_count('HOW ARE YOU? i am great!') 
         {'o': 2, 'a': 3, 'e': 2, 'u': 1, 'i': 1}
     """
+    vowels = 'aeiou'
+    phrase = phrase.lower()
+    frequency_map = {char: phrase.count(char) for char in phrase if char in vowels}
+    return frequency_map
+    
