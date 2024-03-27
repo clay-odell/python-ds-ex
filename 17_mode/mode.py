bcf30d1523d26320a693a5e045ca9793220d28b7
Clay O'Dell <clay.odell.music@gmail.com>
2024-03-27 14:03:06 -0500

    completed mode.py

--- 17_mode/mode.py
+++ 17_mode/mode.py
@@ -11,3 +11,12 @@ def mode(nums):
         >>> mode([2, 2, 3, 3, 2])
         2
     """
+    count_dict = {}
+    for num in nums:
+        if num not in count_dict:
+            count_dict[num] = 1
+        else:
+            count_dict[num] += 1
+    
+    max_key = max(count_dict, key = count_dict.get)
+    return max_key
