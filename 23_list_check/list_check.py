ddc926b9a0061d015726e7a24c7062da6c392ee5
Clay O'Dell <clay.odell.music@gmail.com>
2024-03-27 15:53:42 -0500

    completed list_check.py

--- 23_list_check/list_check.py
+++ 23_list_check/list_check.py
@@ -8,6 +8,7 @@ def list_check(lst):
         False
     """
     for item in lst:
-        if not isinstance (item, list):
-             return False
+        if not isinstance(item, list):
+            return False
     return True
+
