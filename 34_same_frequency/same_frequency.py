cefeb5ff8bb20d8f0d3856e07d3a4cd17b643cad
Clay O'Dell <clay.odell.music@gmail.com>
2024-03-27 19:13:10 -0500

    same_frequency.py completed

--- 34_same_frequency/same_frequency.py
+++ 34_same_frequency/same_frequency.py
@@ -10,3 +10,18 @@ def same_frequency(num1, num2):
         >>> same_frequency(1212, 2211)
         True
     """
+    str_num1 = str(num1)
+    str_num2 = str(num2)
+    dict_num1 = {}
+    dict_num2 = {}
+    for digit in str_num1:
+        if digit in dict_num1:
+            dict_num1[digit] += 1
+        else:
+            dict_num1[digit] = 1
+    for digit in str_num2:
+        if digit in dict_num2:
+            dict_num2[digit] += 1
+        else:
+            dict_num2[digit] = 1
+    return dict_num1 == dict_num2
