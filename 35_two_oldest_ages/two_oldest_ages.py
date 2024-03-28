ad2b7a290e962da897f505254f16ce76e96cb7ae
Clay O'Dell <clay.odell.music@gmail.com>
2024-03-27 19:32:05 -0500

    two_oldest_ages.py completed

--- 35_two_oldest_ages/two_oldest_ages.py
+++ 35_two_oldest_ages/two_oldest_ages.py
@@ -21,3 +21,6 @@ def two_oldest_ages(ages):
     # you may find it helpful to research the `sorted(iter)` function, which
     # can take *any* type of list-like-thing, and returns a new, sorted list
     # from it.
+    ages_set = set(ages)
+    sorted_ages = sorted(ages_set)
+    return tuple(sorted_ages[-2:])
