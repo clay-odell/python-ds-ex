dd6b04f3d75d1ba0e4e33f5dfb031d49fc96a32f
Clay O'Dell <clay.odell.music@gmail.com>
2024-03-27 19:58:55 -0500

    find_the_duplicate.py completed

--- 36_find_the_duplicate/find_the_duplicate.py
+++ 36_find_the_duplicate/find_the_duplicate.py
@@ -13,3 +13,10 @@ def find_the_duplicate(nums):
         >>> find_the_duplicate([2, 1, 3, 4]) is None
         True
     """
+    freq = {}
+    for num in nums:
+        if num in freq:
+            return num
+        else:
+            freq[num] = 1
+    return None
